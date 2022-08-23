7c29ed7609aafb02bf39f0ead73c2a6e3cdb8f97
ErickSilva2605 <erick.dev.it@gmail.com>
2022-08-23 00:51:39 -0300

    Adicionando a primeira activity

---
+++ app/src/main/java/br/com/alura/aluratrips/ui/activity/TravelPackagesList.java
@@ -0,0 +1,16 @@
+package br.com.alura.aluratrips.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.alura.aluratrips.R;
+
+public class TravelPackagesList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel_packages_list);
+    }
+}
