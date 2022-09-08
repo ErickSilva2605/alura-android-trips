4d5491cea3200c753b1f647d13fdbeedd919b30b
ErickSilva2605 <erick.dev.it@gmail.com>
2022-09-08 01:03:00 -0300

    Aplicando bind e refatorando tela de pagamento

--- app/src/main/java/br/com/alura/aluratrips/ui/activity/PaymentActivity.java
+++ app/src/main/java/br/com/alura/aluratrips/ui/activity/PaymentActivity.java
@@ -3,8 +3,13 @@ package br.com.alura.aluratrips.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import br.com.alura.aluratrips.R;
+import br.com.alura.aluratrips.model.TravelPackage;
+import br.com.alura.aluratrips.util.CurrencyUtil;
 
 public class PaymentActivity extends AppCompatActivity {
 
@@ -15,5 +20,19 @@ public class PaymentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
         setTitle(APPBAR_TITLE);
+
+        TravelPackage travelPackage = new TravelPackage("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
+        setFields(travelPackage);
+    }
+
+    private void setFields(TravelPackage travelPackage) {
+        setPrice(travelPackage);
+    }
+
+    private void setPrice(TravelPackage travelPackage) {
+        TextView price = findViewById(R.id.payment_purchase_price);
+        String priceFormatted = CurrencyUtil
+                .formatToPtBR(travelPackage.getPrice());
+        price.setText(priceFormatted);
     }
 }
